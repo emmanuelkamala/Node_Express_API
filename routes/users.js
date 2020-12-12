c1702a9c424867d605591abf812cf0dcaf3ecc2d
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-13 00:33:16 +0300

    Add a post route to the API

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,31 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "Emmanuel",
+    lastName: "Kamala",
+    age: 34
+  },
+
+  {
+    firstName: "Angelina",
+    lastName: "Nyamwihula",
+    age: 31
+  }
+]
+
 router.get('/', (req, res) => {
-  res.send('Hello from users');
+  console.log(users);
+  res.send(users);
+})
+
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+
+  res.send(`User with the name ${user.firstName} added to the database`);
 })
 
 export default router;
